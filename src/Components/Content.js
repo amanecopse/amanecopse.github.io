73f95c33ee2e2617d5a0fafe978d1c552b68ae8d
amanecopse <amanecopse@gmail.com>
2022-01-05 11:40:21 +0900

    Add Education tag and id in Content.js

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,14 +1,16 @@
 import { Col, Container, Row, Button } from "react-bootstrap";
+import Education from "./Contents/Education";
 import Profile from "./Contents/Profile";
 import Space from "./Space";
 
 function Content() {
   return (
-    <Container fluid>
+    <Container fluid id="start">
       <Space size={9}></Space>
 
       <article>
         <Profile></Profile>
+        <Education></Education>
       </article>
     </Container>
   );
